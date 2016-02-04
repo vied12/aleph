2046775b7ab748c1a2ed3a69cad684d100fac908
Dan O'Huiginn <daniel@ohuiginn.net>
2016-02-04 12:13:58 +0100

    Metadata fixes

--- aleph/contrib/openoil/scheduled_updates.py
+++ aleph/contrib/openoil/scheduled_updates.py
@@ -12,7 +12,12 @@ def _crawler(sourcename):
     return _inner
     
 def schedule_updates():
-    schedule.every(3).days.do(_crawler('sec-edgar'))
+    intervals = (
+        (3, 'sec-edgar'),
+        (1, 'sedar-partial-content'),
+       )
+    for (days, source) in intervals:
+        schedule.every(days).days.do(_crawler(source))
 
     while(1):
         schedule.run_pending()
