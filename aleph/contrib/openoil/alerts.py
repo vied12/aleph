619d1d44ea231f2261f6cef0cde9250322ce4716
Dan O'Huiginn <daniel@ohuiginn.net>
2016-02-25 17:21:33 +0100

    First pass at db table for alerts system

---
+++ aleph/contrib/openoil/alerts.py
@@ -0,0 +1,16 @@
+'''
+Aleph email alert system
+
+See documentation at  https://docs.google.com/document/d/1Fw-JaIj_9ds-XYlqFdHvBd8QLPTtpEImt5JWFWYA6hg/edit#heading=h.uyujmyqco778
+
+'''
+
+
+def check_alerts():
+    pass
+
+def add_alert():
+    pass
+
+def mailout():
+    pass
