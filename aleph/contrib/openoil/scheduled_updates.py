441b829b6fb8dc53a035ef0dc1631c69dbfaa5a1
Dan O'Huiginn <daniel@ohuiginn.net>
2016-03-10 15:08:16 +0100

    Scheduled updates should happen overnight

--- aleph/contrib/openoil/scheduled_updates.py
+++ aleph/contrib/openoil/scheduled_updates.py
@@ -6,20 +6,27 @@ import time, datetime
 from aleph.crawlers import crawl_source
 from aleph.core import es, es_index
 
+
+
 def _crawler(sourcename):
     def _inner():
-        crawl_source(sourcename, ignore_tags=False)
-        return schedule.CancelJob
+        crawl_source.delay(sourcename, ignore_tags=False)
+        #return schedule.CancelJob
     return _inner
 
 def schedule_updates():
-    intervals = (
-        (1, 'sec-edgar'),
-        (1, 'sedar-partial-content'),
-       )
-    for (days, source) in intervals:
-        schedule.every(days).days.do(_crawler(source))
 
+    # EDGAR
+    schedule.every(1).days.at("04:30").do(_crawler('sec-edgar'))
+
+    # SEDAR
+    # Sedar website stops updating at 11pm ET, i.e. 0500 CET
+    # We start our scrape just after, at 0511 CET, and allow 3 hours for it to
+    # upload
+    schedule.every(1).days.at("08:00").do(_crawler('sedar-partial-content'))
+
+    schedule.run_all()
+    
     while(1):
         schedule.run_pending()
         time.sleep(1)
